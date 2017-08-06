f59f50b84facb326b8456889bfff709980bb8e52
Michael <michael_georgiev@hotmail.co.uk>
2017-08-06 01:07:57 +0100

    working script ready to run

--- haar_cascade/download_images.py
+++ haar_cascade/download_images.py
@@ -3,30 +3,31 @@ import numpy as np
 import os
 import requests
 import shutil
+from sys import argv
 
-def store_raw_images():
-    negative_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n03319745'
-    r = requests.get(negative_link)
-    negative_urls = r.text.encode("utf-8")
+def store_raw_images(links, img_type):
 
-    if not os.path.exists('neg'):
-        os.makedirs('neg')
+    r = requests.get(links)
+    urls = r.text.encode("utf-8")
+
+    if not os.path.exists(img_type):
+        os.makedirs(img_type)
 
     num = 1
-    split_links = negative_urls.split('\n')
+    split_links = urls.split('\n')
     for url in split_links:
         try:
             print str(num), url
             response = requests.get(url, stream=True)
 
-            complete_path = os.path.join('/home/michael/terrapin/haar_cascade/neg', str(num)+".jpg")
+            complete_path = os.path.join('/home/michael/terrapin/haar_cascade/'+img_type+"/"+str(num)+".jpg")
             with open(complete_path, 'wb') as out_file:
                 shutil.copyfileobj(response.raw, out_file)
             del response
 
             img = cv2.imread(complete_path, cv2.IMREAD_GRAYSCALE)
             resized_img = cv2.resize(img, (100,100))
-            cv2.imwrite("neg/"+str(num)+".jpg", resized_img)
+            cv2.imwrite(img_type+"/"+str(num)+".jpg", resized_img)
 
         except Exception as e:
             print e
@@ -43,9 +44,9 @@ def check_if_white(current_path):
     return True if non_whites < 400 else False
 
 
-def remove_images():
-    for item in os.listdir("neg"):
-        current_path = "neg/" + str(item)
+def remove_images(img_type):
+    for item in os.listdir(img_type):
+        current_path = img_type + "/" + str(item)
         print current_path
         try:
             img = cv2.imread(current_path)
@@ -64,7 +65,20 @@ def remove_images():
         print
 
 
-
-# store_raw_images()
-remove_images()
-# print check_if_white()
+if __name__ == "__main__":
+    if argv[1] == "store":
+        if argv[2] == "pos":
+            positive_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n07928696'
+            store_raw_images(positive_link, argv[2])
+        elif argv[2] == "neg":
+            negative_link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n03319745'
+            store_raw_images(negative_link, argv[2])
+        else:
+            print "Incorrect image type"
+    elif argv[1] == "remove":
+        if argv[2] == "pos":
+            remove_images("pos")
+        elif argv[2] == "neg":
+            remove_images("neg")
+        else:
+            print "Incorrect image type"
